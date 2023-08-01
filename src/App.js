714f56d05e49765c293e27661f2795e261ac1ece
Giuseppe Lanucara <giuseppelanucara96@gmail.com>
2023-08-01 11:06:02 +0200

    feat: creato Cards e inserito img

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import Navbar from './components/navbar';
+import Card from './components/card';
 import './App.css';
 
 function App() {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <div className='container'>
+        <h1>Cosa vuole ordinare oggi?</h1>
+        <hr />
+        <div className='row'>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
+
+    </>
+
+
   );
 }
 
